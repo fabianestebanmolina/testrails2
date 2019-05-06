d3d953b1241b47eda09e95ff71f27725a1f11155
fabian <fabian.makt@gmail.com>
2019-05-05 23:28:02 -0400

    strongparams

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
 
   resources :to_dos
+  get 'users/search'
   root to: 'to_dos#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
